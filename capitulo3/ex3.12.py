17a50a56e8a63a490cb156f253cb795b078476c0
dav1osilva <odavi8926@gmail.com>
2025-10-25 22:01:43 -0300

    Adiciona exercício 12 do capítulo 3

---
+++ capitulo3/ex3.12.py
@@ -0,0 +1,4 @@
+distância = float(input("Qual a distância do seu percurso em km? "))
+velocidade_média = float(input("Qual é a sua estimativa sobre velocidade média em km/h? "))
+tempo = distância / velocidade_média
+print(f"O tempo gasto será de {tempo} h")
