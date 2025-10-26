48da11e04464a52d77beecc02f10f1c59a54b051
dav1osilva <odavi8926@gmail.com>
2025-10-25 22:00:43 -0300

    Adiciona exercício 5 do capítulo 3

---
+++ capitulo3/ex3.5.py
@@ -0,0 +1,17 @@
+a1 = 1
+b1 = 2
+c1 = True
+d1 = False
+print(a1 > b1 and c1 or d1)
+
+a2 = 10
+b2 = 3
+c2 = False
+d2 = False
+print(a2 > b2 and c2 or d2)
+
+a3 = 5
+b3 = 1
+c3 = True
+d3 = True
+print(a3 > b3 and c3 or d3)
