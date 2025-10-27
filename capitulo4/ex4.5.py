0f12e5df7eea908b4380fcd01dc2eda46772ed57
dav1osilva <odavi8926@gmail.com>
2025-10-26 22:01:07 -0300

    Adiciona exercício 5 do capítulo 4

---
+++ capitulo4/ex4.5.py
@@ -0,0 +1,13 @@
+idade = int(input("Quantos anos tem seu carro? "))
+if idade <= 3:
+    print("Seu carro é novo")
+else:
+    print("Seu carro é velho")
+
+# é a mesma coisa do código abaixo, mas é mais fácil de ler
+
+# idade = int(input("Digite a idade do seu carro: "))
+# if idade <= 3:
+#    print("Seu carro é novo") 
+# if idade > 3:
+#    print("Seu carro é velho") 
