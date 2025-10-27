4b01c991a9141ff5619f6623ab2cc8bc38dcc410
dav1osilva <odavi8926@gmail.com>
2025-10-26 22:00:49 -0300

    Adiciona exercício 3 do capítulo 4

---
+++ capitulo4/ex4.3.py
@@ -0,0 +1,20 @@
+n1 = float(input("Digite um valor: "))
+n2 = float(input("Digite outro valor: "))
+n3 = float(input("Digite o último valor: "))
+
+# maior valor
+if n1 > n2 and n1 > n3:
+   print(n1)
+if n2 > n1 and n2 > n3:
+   print(n2)
+if n3 > n1 and n3 > n2:
+   print(n3)
+
+# menor valor
+if n1 < n2 and n1 < n3:
+   print(n1)
+if n2 < n1 and n2 < n3:
+   print(n2)
+if n3 < n1 and n3 < n2:
+   print(n3)
+
