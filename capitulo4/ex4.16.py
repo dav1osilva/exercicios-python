42ab0f544b1f0041a8bf711576b0ea0dcef38af2
dav1osilva <odavi8926@gmail.com>
2025-10-26 22:02:45 -0300

    Adiciona exercício 16 do capítulo 4

---
+++ capitulo4/ex4.16.py
@@ -0,0 +1,8 @@
+média = float(input("Digite sua média: "))
+
+if média < 4:
+    print("Infelizmente você reprovou")
+elif média < 7:
+    print("Você ficou de recuperação")
+else:
+    print("Você passou de ano")
