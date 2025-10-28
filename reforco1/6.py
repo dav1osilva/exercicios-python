9efe30f6d47ae40ba79ace8165423abc19657d6d
dav1osilva <odavi8926@gmail.com>
2025-10-27 21:25:27 -0300

    Adiciona exercício 6 de reforço

---
+++ reforco1/6.py
@@ -0,0 +1,6 @@
+n1 = float(input("Digite a nota: "))
+n2 = float(input("Digite a nota: "))
+n3 = float(input("Digite a nota: "))
+media = (n1 + n2 + n3) / 3
+
+print(f"A média é igual a {media:.2f}")
