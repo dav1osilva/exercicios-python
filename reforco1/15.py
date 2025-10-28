4565dc21b69bc74841517e758b37e827849c0633
dav1osilva <odavi8926@gmail.com>
2025-10-27 21:26:49 -0300

    Adiciona exercício 15 de reforço

---
+++ reforco1/15.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+
+if n1 % n2 == 0:
+    print(f"{n1} é múltiplo de {n2}")
+else:
+    print(f"{n1} não é múltiplo de {n2}")
