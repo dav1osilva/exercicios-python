163794378d4e410d53da6bb8f466cabded912a09
dav1osilva <odavi8926@gmail.com>
2025-10-27 21:25:12 -0300

    Adiciona exercício 4 de reforço

---
+++ reforco1/4.py
@@ -0,0 +1,4 @@
+ano_nascimento = int(input("Digite o ano em que você nasceu: "))
+ano_atual = int(input("Digite o ano atual: ")) 
+idade = ano_atual - ano_nascimento
+print(f"Você tem {idade} anos")
