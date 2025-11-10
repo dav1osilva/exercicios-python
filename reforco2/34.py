4016164e6a2bb12aa066d1a3adbe8d19d10088ab
dav1osilva <odavi8926@gmail.com>
2025-11-09 21:57:02 -0300

    Adiciona exercício 34 do reforço 2

---
+++ reforco2/34.py
@@ -0,0 +1,7 @@
+colunas = int(input("Digite o número de colunas: "))
+linhas = int(input("Digite o número de linhas: ")) 
+contador = 1
+
+while contador <= linhas:
+    print("*" * colunas)
+    contador += 1
